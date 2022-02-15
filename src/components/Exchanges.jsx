3b94d90bb0d4e84d6993110a0f154eb630098dfd
Amirhossein Ghasemzadeh <amirghasemzadeh17@gmail.com>
2022-02-15 18:53:18 +0330

    saved

---
+++ src/components/Exchanges.jsx
@@ -0,0 +1,16 @@
+import { Row, Col } from 'antd';
+
+const Exchanges = () => {
+  return (
+    <>
+      <Row>
+        <Col span={6}>Exchanges</Col>
+        <Col span={6}>24h Trade Volume</Col>
+        <Col span={6}>Markets</Col>
+        <Col span={6}>Change</Col>
+      </Row>
+    </>
+  );
+};
+
+export default Exchanges;
